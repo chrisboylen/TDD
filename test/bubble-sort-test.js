438ad0f9cc0d042833bd32885d9ba661fcebb6c2
Chris Boylen <billpolster@gmail.com>
2018-05-10 13:38:42 -0600

    Add test bubbleSort for bubble-sort-test.js

--- test/bubble-sort-test.js
+++ test/bubble-sort-test.js
@@ -1,14 +1,11 @@
 const chai = require('chai');
+const { expect } = require('chai');
 const assert = chai.assert;
 const bubbleSort = require('../lib/bubble-sort.js');
 
-describe('bubbleSort', () => {
-    let array;
-    beforeEach(() => {
-        array = [1, 2, 3, 4, 5];
-    })
-
-    it('should have the bubbleSort function'), () => {
-        assert.isFunction(bubbleSort)
-    })
-};
+describe('bubbleSort()', () => {
+    it('should sort an array of numbers from least to greatest', () => {
+        let array = [2, 1, 0, 3, 5, 4, 10, 7, 6, 8, 9];
+        expect(bubbleSort(array)).to.deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+});
