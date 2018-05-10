51c99a99283923f08a5a061a4fd5fb768a94a3a1
Chris Boylen <billpolster@gmail.com>
2018-05-10 14:00:34 -0600

    Add test to insertion-sort-test.js

---
+++ test/insertion-sort-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const { expect } = require('chai');
+const assert = chai.assert;
+const insertionSort = require('../lib/insertion-sort.js');
+
+describe('insertionSort()', () => {
+    it('should sort an array of numbers from least to greatest', () => {
+        let array = [2, 1, 0, 3, 5, 4, 10, 7, 6, 8, 9];
+        expect(insertionSort(array)).to.deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+});
